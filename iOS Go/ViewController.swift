e56d2ca3a87381ed50dc4c1a63668c4f3c1c23e8
Donggaem <ehdrua465111@naver.com>
2022-05-10 21:58:15 +0900

    Initial Commit

---
+++ iOS Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Go
+//
+//  Created by 김동겸 on 2022/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
