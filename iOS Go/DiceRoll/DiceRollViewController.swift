82ee1f4af6edab8a2238ca791c6cbae7fa9f19fd
Donggaem <ehdrua465111@naver.com>
2022-05-11 01:43:57 +0900

    DiceRoll 버튼 클릭시 주사위 굴리기(기본 변수, 상수, ,배열, 난수발생)

---
+++ iOS Go/DiceRoll/DiceRollViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DiceViewController.swift
+//  iOS Go
+//
+//  Created by 김동겸 on 2022/05/10.
+//
+import UIKit
+
+class DiceRollViewController: UIViewController {
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+    }
+}
+
