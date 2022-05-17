f14de6b59c0ce208816f0740ebbe1c9b7de2b1f5
Donggaem <ehdrua465111@naver.com>
2022-05-17 23:44:52 +0900

    Quizzer (MVC패턴 적용, 구조체 활용)

---
+++ iOS Go/Quizzler/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  iOS Go
+//
+//  Created by 김동겸 on 2022/05/17.
+//
+
+import Foundation
+
+struct Question{
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String){
+        text = q
+        answer = a
+        
+    }
+}
