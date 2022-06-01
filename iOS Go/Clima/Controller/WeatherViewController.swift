1d330f3f08ffffe03d96643b9c91238829a37856
Donggaem <ehdrua465111@naver.com>
2022-06-01 13:49:05 +0900

    Clima 날씨앱 (다크모드 적용시 변화,  UITextField 사용)

---
+++ iOS Go/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherViewController.swift
+//  iOS Go
+//
+//  Created by 김동겸 on 2022/06/01.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var seachTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        seachTextField.delegate = self
+    }
+
+    @IBAction func seachPressed(_ sender: UIButton) {
+        seachTextField.endEditing(true)
+        print(seachTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        seachTextField.endEditing(true)
+        print(seachTextField.text)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    //편집이 종료된 텍스트필드
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        seachTextField.text = ""
+    }
+}
